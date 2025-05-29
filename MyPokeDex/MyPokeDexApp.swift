8e2ba251c24653525b6c97b8791ce72a8680ae8a
German <german220291@gmail.com>
2025-05-29 14:09:36 +0100

    Initial Commit

---
+++ MyPokeDex/MyPokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokeDexApp.swift
+//  MyPokeDex
+//
+//  Created by Germán Hernández del Rosario on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPokeDexTests/MyPokeDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokeDexTests.swift
+//  MyPokeDexTests
+//
+//  Created by Germán Hernández del Rosario on 29/5/25.
+//
+
+import Testing
+@testable import MyPokeDex
+
+struct MyPokeDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
